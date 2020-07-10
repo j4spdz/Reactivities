e4aa1c4d71aa3a0783c7afc7b4b9e0049da4412a
j4spdz <code.jack4spadesz@gmail.com>
2020-07-10 14:21:41 +0800

    Added basic crud for activities

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -31,7 +29,7 @@ namespace DatingApp.API.Controllers
     [HttpGet("{id}")]
     public async Task<ActionResult<string>> Get(int id)
     {
-			var value = await _context.Values.FindAsync(id);
+      var value = await _context.Values.FindAsync(id);
       return Ok(value);
     }
 
