31ccb2818272550fc013511d4fff577bae5e6e6a
j4spdz <code.jack4spadesz@gmail.com>
2020-07-19 16:29:16 +0800

    Added custom authorization that only host can edit or delete an activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
-using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -30,6 +29,7 @@ namespace API.Controllers
     }
 
     [HttpPut("{id}")]
+    [Authorize(Policy = "IsActivityHost")]
     public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
     {
       command.Id = id;
@@ -37,6 +37,7 @@ namespace API.Controllers
     }
 
     [HttpDelete("{id}")]
+    [Authorize(Policy = "IsActivityHost")]
     public async Task<ActionResult<Unit>> Delete(Guid id)
     {
       return await Mediator.Send(new Delete.Command { Id = id });
