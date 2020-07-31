0d44921b75128c043d24c2779cef6a2d5c9dc2ca
j4spdz <code.jack4spadesz@gmail.com>
2020-07-31 21:23:33 +0800

    Added infinite scrolling to activities page

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,31 +1,55 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 
-import { Grid } from 'semantic-ui-react';
+import { Grid, Button, Loader } from 'semantic-ui-react';
 
 import ActivityList from './ActivityList';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 import { observer } from 'mobx-react-lite';
 import { RootStoreContext } from '../../../app/stores/rootStore';
+import InfiniteScroll from 'react-infinite-scroller';
 
 const ActivityDashboard: React.FC = () => {
 	const rootStore = useContext(RootStoreContext);
-	const { loadActivites, loadingInitial } = rootStore.activityStore;
+	const {
+		loadActivites,
+		loadingInitial,
+		setPage,
+		page,
+		totalPages,
+	} = rootStore.activityStore;
+	const [loadingNext, setLoadingNext] = useState(false);
+
+	const handleGetNext = () => {
+		setLoadingNext(true);
+		setPage(page + 1);
+		loadActivites().then(() => setLoadingNext(false));
+	};
 
 	useEffect(() => {
 		loadActivites();
 	}, [loadActivites]);
 
-	if (loadingInitial)
+	if (loadingInitial && page === 0)
 		return <LoadingComponent content='Loading activities...' />;
 
 	return (
 		<Grid>
 			<Grid.Column width={10}>
-				<ActivityList />
+				<InfiniteScroll
+					pageStart={0}
+					loadMore={handleGetNext}
+					hasMore={!loadingNext && page + 1 < totalPages}
+					initialLoad={false}
+				>
+					<ActivityList />
+				</InfiniteScroll>
 			</Grid.Column>
 			<Grid.Column width={6}>
 				<h2>Activity filters</h2>
+			</Grid.Column>
+			<Grid.Column width={10}>
+				<Loader active={loadingNext} />
 			</Grid.Column>
 		</Grid>
 	);
